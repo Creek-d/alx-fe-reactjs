a4dc640808691fecaac944317f75ad49bce2a659
david chege <mutmark8@gmail.com>
2025-02-13 02:36:25 +0300

    all

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,15 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+}
 
-  return (
-    <div>
-      <WelcomeMessage />
-    </div>
-  )
-};
-
-export default App
+export default App;
