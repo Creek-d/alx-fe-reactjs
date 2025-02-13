264c3ff58a96a395e2869496f5521ff5618b0c6b
david chege <mutmark8@gmail.com>
2025-02-13 03:13:54 +0300

    all

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,22 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'; // Import UserProfile
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      
+      {/* Add UserProfile component here */}
+      <UserProfile name="David" age="31" bio="Loves hiking and biking" />
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
