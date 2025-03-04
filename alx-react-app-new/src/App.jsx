28414197c3e0d60797c39d17ec52e9dc4c65c13c
Creek-d <mutmark8@gmail.com>
2025-03-05 02:17:44 +0300

    all

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile'; // Import UserProfile
+import Counter from './components/Counter'; 
+
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       
       {/* Add UserProfile component here */}
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      {/* Add the Counter component here */}
+      <Counter /> {/* This will display the counter */}
 
       <Footer />
     </div>
