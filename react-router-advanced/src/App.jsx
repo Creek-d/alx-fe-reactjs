50266ae56c600bd58fb0acfce4f8c13d5e01b77a
Creek-d <mutmark8@gmail.com>
2025-03-08 04:45:36 +0300

    all

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,28 +1,30 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Profile from "./components/Profile.jsx";  // ✅ Updated path
+import Profile from "./components/Profile.jsx";
 import BlogPost from "./pages/BlogPost.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import Login from "./pages/Login.jsx";
 
 function App() {
   return (
-    <div>
-      <h1>React Router Advanced</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <BrowserRouter>
+      <div>
+        <h1>React Router Advanced</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        {/* Protected Route for Profile */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/profile/*" element={<Profile />} />
-        </Route>
+          {/* Protected Route for Profile */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/profile/*" element={<Profile />} />
+          </Route>
 
-        {/* Dynamic Routing for Blog Posts */}
-        <Route path="/blog/:postId" element={<BlogPost />} />
+          {/* Dynamic Routing for Blog Posts */}
+          <Route path="/blog/:postId" element={<BlogPost />} />
 
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
