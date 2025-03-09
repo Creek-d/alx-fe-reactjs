2c0499021af4c695543c105d135530173fd1e70b
Creek-d <mutmark8@gmail.com>
2025-03-10 01:21:37 +0300

    all

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,18 +5,35 @@ const useRecipeStore = create((set) => ({
   searchTerm: '',
   filteredRecipes: [],
 
-  // Set the search term
+  // Add a new recipe to the list
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe],
+  })),
+
+  // Set the entire list of recipes
+  setRecipes: (recipes) => set({ recipes }),
+
+  // Update an existing recipe
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
+
+  // Delete a recipe
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+  })),
+
+  // Set search term for filtering
   setSearchTerm: (term) => set({ searchTerm: term }),
 
   // Filter recipes based on search term
-  filterRecipes: () =>
-    set((state) => ({
-      filteredRecipes: state.recipes.filter((recipe) =>
-        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
-      ),
-    })),
-
-  // You can also add other filtering criteria like ingredients, preparation time, etc.
+  filterRecipes: () => set((state) => ({
+    filteredRecipes: state.recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    ),
+  })),
 }));
 
 export default useRecipeStore;
